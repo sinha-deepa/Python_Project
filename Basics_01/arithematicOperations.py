06168ccfad299eafde1ae508ed500eee5db26141
sinha-deepa <deeps.lata24@gmail.com>
2026-01-01 11:52:25 +0530

    Python basics - Variable and arithematic Operations

---
+++ Basics_01/arithematicOperations.py
@@ -0,0 +1,11 @@
+a=12
+b=23.5
+c=45.67
+sum = a+b+c
+diff = c-b
+prod=a*b*c
+div=45.67/12
+print("sum", sum)
+print("difference", diff)
+print("product", prod)
+print("division", div)
---
+++ Basics_01/variableDatatypeDemo.py
@@ -0,0 +1,22 @@
+a=10
+b=23.5
+c="Coding is fun!"
+d='D'
+e=True
+x,y,z = 10, 12.56, 'Fun'
+print("a = ",a)
+print("b = ",b)
+print("c = ",c)
+print("d = ",d)
+print("e = ",e)
+print("x = ",x)
+print("y = ",y)
+print("z = ",z)
+print("Data type of a is ",type(a))
+print("Data type of b is ",type(b))
+print("Data type of c is ",type(c))
+print("Data type of d is ",type(d))
+print("Data type of e is ",type(e))
+print("Data type of x is ",type(x))
+print("Data type of y is ",type(y))
+print("Data type of z is ",type(z))
